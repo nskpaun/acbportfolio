5860aa40f052ed071485187b049d542e6a67d352
nskpaun <nkspaun@gmail.com>
2015-04-30 11:24:03 -0700

    adding projects with multiple slides

--- controllers/projectform.js
+++ controllers/projectform.js
@@ -7,12 +7,22 @@ exports.getForm = function(req, res) {
 };
 
 exports.postProject = function(req, res, next) {
+	var slides = [];
+	var texts = req.body.ptext;
+	var urls = req.body.purl;
+	for (i = 0; i < texts.length; i ++) {
+		var ptext = req.body.ptext[i];
+		var purl = req.body.purl[i];
+		if (purl.length > 0 && ptext.length > 0) {
+			slides.push({
+				text: req.body.ptext[i],
+				image: req.body.purl[i]
+			})
+		}
+	}
 	var project = new Project({
 		name: req.body.pname,
-		slide: [{
-			text: req.body.ptext,
-			image: req.body.purl
-		}]
+		slide: slides
 	});
 
 	project.save(function(err) {
