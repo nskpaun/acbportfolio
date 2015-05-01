bd9eccd6ed0c77a5cd8ec8b18030b5dba94e5500
nskpaun <nkspaun@gmail.com>
2015-05-01 11:37:20 -0700

    clickable points on the edge

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,42 @@
 $(document).ready(function() {
 
+  var closeAll = function() {
+  	for(i = 0; i < 7; i++) {
+  	  $('#content0'+i).css('visibility','hidden');
+  	}
+  }
+
+  var showContent = function(contentIndex) {
+  	closeAll();
+  	$('#content0'+contentIndex).css('visibility','visible');
+  }
+
   // Place JavaScript code here...
   $('.deg3').click(function () {
-    alert("hello world");
+    showContent(0);
   });
+  $('.deg54').click(function () {
+    showContent(1);
+  });
+  $('.deg105').click(function () {
+    showContent(2);
+  });
+  $('.deg156').click(function () {
+    showContent(3);
+  });
+  $('.deg207').click(function () {
+    showContent(4);
+  });
+  $('.deg258').click(function () {
+    showContent(5);
+  });
+  $('.deg309').click(function () {
+    showContent(6);
+  });
+
+  $('.close-button').click(function() {
+  	closeAll();
+  })
+
 
 });
