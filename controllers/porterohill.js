5be338cd16cffd39d8092384907afcad8bce4caf
nskpaun <nkspaun@gmail.com>
2015-05-01 13:23:31 -0700

    Connect database to project layout

--- controllers/porterohill.js
+++ controllers/porterohill.js
@@ -1,11 +1,10 @@
 var Project = require('../models/Project.js')
 
 exports.getProject = function(req, res) {
-	Project.find(function(err, docs){
+	Project.find({name: 'Portero Hill'},function(err, docs){
 		res.render('project', {
 		    title: 'Portero Hill',
 		    projects: docs
 		  });
 	})
-  
 };
